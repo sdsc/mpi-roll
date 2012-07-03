4c8ae02a0e24736bc1e748be746277a592ea868e
Jerry Greeenberg <jpg@sdsc.edu>
2012-07-03 23:26:33 +0000

    release 2

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,5 +1,5 @@
 NAME    = roll-$(ROLL)-usersguide
-RELEASE = 0
+RELEASE = 2
 
 SUMMARY_COMPATIBLE   = $(VERSION)
 SUMMARY_MAINTAINER   = Rocks Group
--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 ROLLNAME = mpi$(ROLLSUFFIX)
-RELEASE  = 0
+RELEASE  = 2
 COLOR    = orange
 
 REDHAT.ROOT = $(PWD)
