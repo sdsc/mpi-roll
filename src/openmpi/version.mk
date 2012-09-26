f137488900fc7459201d842eaa6a0787693243ca
Philip Papadopoulos <phil@sdsc.edu>
2012-09-26 18:22:18 +0000

    Support building for SGE or Torque

--- src/openmpi/version.mk
+++ src/openmpi/version.mk
@@ -1,3 +1,10 @@
 NAME    = openmpi_$(ROLLCOMPILER)_$(ROLLNETWORK)
 VERSION = 1.6
 RELEASE = 4
+LOADMGR =
+ifdef SGE
+LOADMGR += --with-sge
+endif
+ifdef TORQUE
+LOADMGR += --with-tm /opt/torque
+endif
