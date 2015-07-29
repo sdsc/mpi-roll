2027df36aff0d9d38012717f2e30b71cd13e0a0d
Jim Hayes <jhayes@sdsc.edu>
2015-07-29 16:42:58 -0700

    Make sure each mpi package is built w/the corresponding limic/knem.

--- src/openmpi/version.mk
+++ src/openmpi/version.mk
@@ -9,7 +9,7 @@ endif
 
 NAME           = sdsc-openmpi_$(COMPILERNAME)_$(ROLLNETWORK)
 VERSION        = 1.8.4
-RELEASE        = 1
+RELEASE        = 2
 PKGROOT        = /opt/openmpi/$(COMPILERNAME)/$(ROLLNETWORK)
 
 SRC_SUBDIR     = openmpi
