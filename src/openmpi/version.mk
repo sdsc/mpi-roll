fe85695a7870ecb97ba07554c4157676d92a6ef6
Jim Hayes <jhayes@sdsc.edu>
2015-09-07 14:15:57 -0700

    Bump release due to prior Makefile commit.

--- src/openmpi/version.mk
+++ src/openmpi/version.mk
@@ -9,7 +9,7 @@ endif
 
 NAME           = sdsc-openmpi_$(COMPILERNAME)_$(ROLLNETWORK)
 VERSION        = 1.8.4
-RELEASE        = 2
+RELEASE        = 3
 PKGROOT        = /opt/openmpi/$(COMPILERNAME)/$(ROLLNETWORK)
 
 SRC_SUBDIR     = openmpi
